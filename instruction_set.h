266d1fff2cec9210aec58ceaf3855504363d54ac
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2009-01-15 01:36:57 -0300

    Added instruction codes Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

---
+++ instruction_set.h
@@ -0,0 +1,64 @@
+#ifndef instruction_set_h
+#define instruction_set_h
+
+#define INSTRUCTION_NUMBER 56
+
+/* 6502 instructions */
+#define ADC 0
+#define AND 1
+#define ASL 2
+#define BCC 3
+#define BCS 4
+#define BEQ 5
+#define BIT 6
+#define BMI 7
+#define BNE 8
+#define BPL 9
+#define BRK 10
+#define BVC 11
+#define BVS 12
+#define CLC 13
+#define CLD 14
+#define CLI 15
+#define CLV 16
+#define CMP 17
+#define CPX 18
+#define CPY 19
+#define DEC 20
+#define DEX 21
+#define DEY 22
+#define EOR 23
+#define INC 24
+#define INX 25
+#define INY 26
+#define JMP 27
+#define JSR 28
+#define LDA 29
+#define LDY 30
+#define LDY 31
+#define LSR 32
+#define NOP 33
+#define ORA 34
+#define PHA 35
+#define PHP 36
+#define PLA 37
+#define PLP 38
+#define ROL 39
+#define ROR 40
+#define RTI 41
+#define RTS 42
+#define SBC 43
+#define SEC 44
+#define SED 45
+#define SEI 46
+#define STA 47
+#define STX 48
+#define STY 49
+#define TAX 50
+#define TAY 51
+#define TSX 52
+#define TXA 53
+#define TXS 54
+#define TYA 55
+
+#endif /* instruction_set_h */
