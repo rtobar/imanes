0323190f74e06d747f7740f9fe13d9dfdc583f23
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2009-01-18 15:47:33 -0300

    Adding interpretation of some addressing modes Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

--- loop.c
+++ loop.c
@@ -8,15 +8,56 @@
 void main_loop(ines_file *file) {
 
 	uint8_t opcode;
+	uint8_t operand;
+	uint16_t abs_address;
 	instruction inst;
 	CPU->PC = file->rom;
 
 	/* This is the main loop */
 	while(1) {
 
+		/* Read opcode and full instruction :) */
 		opcode = *(CPU->PC);
 		inst = instructions[opcode];
-		printf("Instruction is %02x, size %d bytes\n", inst.opcode, inst.size);
+
+		switch( inst.addr_mode ) {
+
+			case ADDR_IMMEDIATE:
+				operand = *(CPU->PC + 1);
+				break;
+
+			case ADDR_ABSOLUTE:
+				abs_address = (*(CPU->PC + 1) << 4) | *(CPU->PC + 2);
+				break;
+
+			case ADDR_ZEROPAGE:
+				abs_address = *(CPU->PC + 1);
+				break;
+
+			case ADDR_IMPLIED:
+				break;
+
+			case ADDR_INDIRECT:
+				break;
+
+			case ADDR_ABS_INDX:
+				abs_address = ((*(CPU->PC + 1) << 4) | *(CPU->PC + 2)) + CPU->X;
+				break;
+
+			case ADDR_ABS_INDY:
+				abs_address = ((*(CPU->PC + 1) << 4) | *(CPU->PC + 2)) + CPU->Y;
+				break;
+
+			case ADDR_ZERO_INDX:
+			case ADDR_ZERO_INDY:
+			case ADDR_IND_INDIR:
+			case ADDR_INDIR_IND:
+			case ADDR_RELATIVE:
+				break;
+
+		}
+
+		
 		CPU->PC += inst.size;
 	}
 
