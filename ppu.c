7a7b6b41d5f65f2c63b8e0fd413cc02a78979c4d
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2009-02-10 20:47:04 -0300

    Fliping sprites horizontally if needed Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

--- ppu.c
+++ ppu.c
@@ -128,8 +128,16 @@ void draw_line(int line) {
 					col_index = ((byte1>>(7-tx))&0x1) | (((byte2>>(7-tx))&0x1)<<1);
 					col_index |=  (byte3&0x03) << 2;
 
-					if( col_index & 0x03 )
-						draw_pixel( tmp + tx, line, system_palette[*(PPU->VRAM + 0x3F10 + col_index)]);
+					/* Don't draw background colors! */
+					if( col_index & 0x03 ) {
+
+						/* Horizontal flip? */
+						if( byte3 & 0x40 )
+							draw_pixel( tmp + 7 - tx, line, system_palette[*(PPU->VRAM + 0x3F10 + col_index)]);
+						else
+							draw_pixel( tmp + tx, line, system_palette[*(PPU->VRAM + 0x3F10 + col_index)]);
+					}
+
 				}
 				
 			}
