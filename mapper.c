a4b687b3e2b0b056acecf6db18d0f7b923ef0ff5
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2009-02-11 15:30:53 -0300

    ooops, cnrom id was 2, not 3 Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

--- mapper.c
+++ mapper.c
@@ -9,6 +9,6 @@ nes_mapper *mapper;
 
 nes_mapper mapper_list[] = {
 	{ 0 , "NROM"  , nrom_check_address  , nrom_switch_banks } ,
-	{ 3 , "CNROM" , cnrom_check_address , cnrom_switch_banks } ,
+	{ 2 , "CNROM" , cnrom_check_address , cnrom_switch_banks } ,
 	{ -1 , NULL , NULL, NULL }
 };
