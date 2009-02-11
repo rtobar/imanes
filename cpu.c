bbc918aa31d62e791e60d01e8097ebc422ebf453
Jorge Avarias Alfaro <javarias@csrg.inf.utfsm.cl>
2009-02-11 16:21:43 -0300

    Removed ram initialization from cpu. Now this functionality is delegated to the mapper
    Signed-off-by: Jorge Avarias Alfaro <javarias@csrg.inf.utfsm.cl>

--- cpu.c
+++ cpu.c
@@ -51,20 +51,7 @@ void dump_stack() {
 }
 
 void init_cpu_ram(ines_file *file) {
-
-	/* 1 ROM bank games load twice to ensure vector tables */
-	/* Free the file ROM (we don't need it anymore) */
-	if( file->romBanks == 1 ) {
-		memcpy( CPU->RAM + 0x8000, file->rom, ROM_BANK_SIZE);
-		memcpy( CPU->RAM + 0xC000, CPU->RAM + 0x8000, ROM_BANK_SIZE);
-	}
-	/* 2 ROM bank games load one in 0x8000 and other in 0xC000 */
-	/* Free the file ROM (we don't need it anymore) */
-	else if (file->romBanks == 2 ) {
-		memcpy( CPU->RAM + 0x8000, file->rom, ROM_BANK_SIZE);
-		memcpy( CPU->RAM + 0xC000, file->rom + ROM_BANK_SIZE, ROM_BANK_SIZE);
-	}
-
+	mapper->reset();
 }
 
 void execute_instruction(instruction inst, operand oper) {
--- nrom.c
+++ nrom.c
@@ -9,8 +9,19 @@ inline void nrom_switch_banks() {
 	return;
 }
 
-/* This shouldn't be called never, anyways */
 void nrom_reset()
 {
+   /* 1 ROM bank games load twice to ensure vector tables */
+   /* Free the file ROM (we don't need it anymore) */
+   if( file->romBanks == 1 ) {
+      memcpy( CPU->RAM + 0x8000, file->rom, ROM_BANK_SIZE);
+      memcpy( CPU->RAM + 0xC000, CPU->RAM + 0x8000, ROM_BANK_SIZE);
+   }
+   /* 2 ROM bank games load one in 0x8000 and other in 0xC000 */
+   /* Free the file ROM (we don't need it anymore) */
+   else if (file->romBanks == 2 ) {
+      memcpy( CPU->RAM + 0x8000, file->rom, ROM_BANK_SIZE);
+      memcpy( CPU->RAM + 0xC000, file->rom + ROM_BANK_SIZE, ROM_BANK_SIZE);
+   }
 	return;
 }
