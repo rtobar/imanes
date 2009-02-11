148af0862400af677c0dacd5fe1a765900aaf8ce
Jorge Avarias Alfaro <javarias@csrg.inf.utfsm.cl>
2009-02-11 16:30:23 -0300

    Added a missed include file in nrom implementation
    Signed-off-by: Jorge Avarias Alfaro <javarias@csrg.inf.utfsm.cl>

--- nrom.c
+++ nrom.c
@@ -1,4 +1,5 @@
 #include "nrom.h"
+#include "cpu.h"
 
 inline int nrom_check_address(uint16_t address) {
 	return 0;
@@ -13,15 +14,15 @@ void nrom_reset()
 {
    /* 1 ROM bank games load twice to ensure vector tables */
    /* Free the file ROM (we don't need it anymore) */
-   if( file->romBanks == 1 ) {
-      memcpy( CPU->RAM + 0x8000, file->rom, ROM_BANK_SIZE);
+   if( mapper->file->romBanks == 1 ) {
+      memcpy( CPU->RAM + 0x8000, mapper->file->rom, ROM_BANK_SIZE);
       memcpy( CPU->RAM + 0xC000, CPU->RAM + 0x8000, ROM_BANK_SIZE);
    }
    /* 2 ROM bank games load one in 0x8000 and other in 0xC000 */
    /* Free the file ROM (we don't need it anymore) */
-   else if (file->romBanks == 2 ) {
-      memcpy( CPU->RAM + 0x8000, file->rom, ROM_BANK_SIZE);
-      memcpy( CPU->RAM + 0xC000, file->rom + ROM_BANK_SIZE, ROM_BANK_SIZE);
+   else if (mapper->file->romBanks == 2 ) {
+      memcpy( CPU->RAM + 0x8000, mapper->file->rom, ROM_BANK_SIZE);
+      memcpy( CPU->RAM + 0xC000, mapper->file->rom + ROM_BANK_SIZE, ROM_BANK_SIZE);
    }
 	return;
 }
