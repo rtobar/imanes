1b086b5bd085181907b57f7de36c2b6d3d9aa298
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2009-02-11 17:05:40 -0300

    Oops, warning there... Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

--- mapper.c
+++ mapper.c
@@ -8,8 +8,8 @@
 nes_mapper *mapper;
 
 nes_mapper mapper_list[] = {
-	{ 0 , "NROM"  , nrom_check_address  , nrom_switch_banks , nrom_reset } ,
+	{ 0 , "NROM"  , nrom_check_address  , nrom_switch_banks , nrom_reset  } ,
 	{ 2 , "CNROM" , cnrom_check_address , cnrom_switch_banks, cnrom_reset } ,
 	{ 3 , "UNROM" , unrom_check_address , unrom_switch_banks, unrom_reset } ,
-	{ -1 , NULL , NULL, NULL }
+	{ -1 }
 };
