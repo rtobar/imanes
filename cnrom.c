529fd72617b0931c8cab77b2656dc22d8b561b1d
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2009-02-16 18:49:25 -0300

    Just adjusting code lines Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

--- cnrom.c
+++ cnrom.c
@@ -17,8 +17,10 @@ int cnrom_check_address(uint16_t address) {
 }
 
 void cnrom_switch_banks() {	
+
 	/* Copy the VROM bank to the 0x0000 of VRAM */
-	memcpy(PPU->VRAM, mapper->file->vrom + mapper->reg1*VROM_BANK_SIZE , VROM_BANK_SIZE);
+	memcpy(PPU->VRAM, mapper->file->vrom + mapper->reg1*VROM_BANK_SIZE,
+	       VROM_BANK_SIZE);
 
 }
 
@@ -26,6 +28,6 @@ void cnrom_reset()
 {
 	memcpy(CPU->RAM+0x8000, mapper->file->rom, ROM_BANK_SIZE);
 	memcpy(CPU->RAM+0xC000,
-			mapper->file->rom + (mapper->file->romBanks-1)*ROM_BANK_SIZE, 
-			ROM_BANK_SIZE);
+	       mapper->file->rom + (mapper->file->romBanks-1)*ROM_BANK_SIZE, 
+	       ROM_BANK_SIZE);
 }
