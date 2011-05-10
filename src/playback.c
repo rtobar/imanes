2327b5ac66d83a15c91bc0eefce2c907e45b5ebc
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2011-05-10 10:36:57 +0200

    Fixed several details on the playback mechanism, like sample removal from the queues, and how it is decided which samples from the different queues participate in the final mix Signed-off-by: Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>

--- src/playback.c
+++ src/playback.c
@@ -46,14 +46,14 @@ static Uint8 tnd_dac_outputs[204];
 void initialize_playback() {
 
 	int i;
-	int max_vol;
+	uint16_t max_vol = 0x00;
 	SDL_AudioSpec desired;
 
 	if( config.sound_mute )
 		return;
 
 	/* Initial parameters for the SDL Audio subsytem */
-	desired.freq     = 22050;
+	desired.freq     = 44100;
 	desired.format   = AUDIO_U8;
 	desired.channels = 1;
 	desired.samples  = 1024;
@@ -78,11 +78,23 @@ void initialize_playback() {
 
 	/* Calculate our own final DAC outputs from the normalized floating ones.
 	 * We do this to avoid floating point calculations during ImaNES loop */
-	max_vol = 0xFF;
+	if( audio_spec.format == AUDIO_U8 || audio_spec.format == AUDIO_S8 )
+		max_vol = 0xFF;
+	else if( audio_spec.format == AUDIO_U16 || audio_spec.format == AUDIO_S16 )
+		max_vol = 0xFFFF;
+
 	for(i=0;i!=32;i++)
-		square_dac_outputs[i] = normal_square_dac_outputs[i]*max_vol;
+		square_dac_outputs[i] = normal_square_dac_outputs[i]*(float)max_vol;
 	for(i=0;i!=204;i++)
-		tnd_dac_outputs[i] = normal_tnd_dac_outputs[i]*max_vol;
+		tnd_dac_outputs[i] = normal_tnd_dac_outputs[i]*(float)max_vol;
+
+	printf("Square outputs:");
+	for(i=0;i!=32;i++)
+		printf(" %u", square_dac_outputs[i]);
+	printf("\nTriangle, DMC, Noise outputs:");
+	for(i=0;i!=204;i++)
+		printf(" %u", tnd_dac_outputs[i]);
+	printf("\n");
 
 	/* DAC queues */
 	dac[0] = NULL;
@@ -98,27 +110,33 @@ void playback_fill_sound_card(void *userdata, Uint8 *stream, int len) {
 
 	/* static variables to keep history of things */
 	static struct timespec previousTime = {0, 0};
+	static unsigned int calls_per_sec = 0;
 	static unsigned long int previous_ppu_cycles = 0;
 
 	Uint8 sample;
 	int pos;
 	unsigned int channel;
+	unsigned int removed;
 	unsigned int length;
 	unsigned long int ppu_cycles;
 	unsigned long int ppu_steps_per_sample;
 	unsigned long int step_ppu_cycles;
 	struct timespec currentTime;
 
+	uint8_t index;
 	uint8_t square1_sample;
 	uint8_t square2_sample;
 	uint8_t triangle_sample;
+	uint8_t noise_sample;
+	uint8_t dmc_sample;
 
 	ppu_cycles = CLK->ppu_cycles;
+	calls_per_sec++;
 
 	clock_gettime(CLOCK_REALTIME, &currentTime);
 	if( currentTime.tv_sec != previousTime.tv_sec ) {
 
-		printf("Elements in queue:");
+		printf("Calls/s: %u, Len is %d. Elements in queue:", calls_per_sec, len);
 
 		length = queue_length(dac[Square1]);
 		if( length != 0 )
@@ -138,46 +156,72 @@ void playback_fill_sound_card(void *userdata, Uint8 *stream, int len) {
 
 		length = queue_length(dac[DMC]);
 		if( length != 0 )
-			printf(" DMC: %d\n", length);
+			printf(" DMC: %d", length);
 
 		printf("\n");
 
+		calls_per_sec = 0;
 	}
-
-	/* Fill with silence at the start */
-	memset(stream, audio_spec.silence, len);
 
 	ppu_steps_per_sample = (ppu_cycles - previous_ppu_cycles)/len;
 
 	/* See the latest DAC outputs and combine them as long as we need */
-	step_ppu_cycles = previous_ppu_cycles;
+	step_ppu_cycles = previous_ppu_cycles + ppu_steps_per_sample;
+
+	/* printf("%lu cycles have passed, we'll play them in %u steps\n", ppu_cycles - previous_ppu_cycles, len); */
 	for(pos=0; pos!=len; pos++) {
 
-		/* Check which sounds should be checked
-		 * For this, we check that the front of every queue is
-		 * the one that should get played, and then we take all the fronts,
-		 * and combine them
-		 */
+		/* Check which sample should be played in this step for each channel */
 		for(channel = 0; channel != 5; channel++) {
-			while(dac[channel] != NULL && dac[channel]->ppu_cycles < step_ppu_cycles)
-				dac[channel] = pop(dac[channel]);
+
+			removed = 0;
+			while(1) {
+
+				/* If there are no samples, do nothing */
+				if( dac[channel] == NULL || dac[channel]->next == NULL )
+					break;
+
+				/* If there is a next sample, check whether we should play that one
+				 * instead of the one placed in the head of the queue. If we're ok,
+				 * we finish checking */
+				if( dac[channel]->next->ppu_cycles <= step_ppu_cycles ) {
+					dac[channel] = pop(dac[channel]);
+					removed++;
+				}
+				else
+					break;
+
+			}
+			if( removed > 1 )
+				printf("Removed %u samples from %u's channel DAC queue\n", removed, channel);
 		}
 
-		sample = 0;
+		/* Now combine the samples, if the corresponding channel is enabled */
 		square1_sample = 0;
 		square2_sample = 0;
 		triangle_sample = 0;
+		noise_sample = 0;
+		dmc_sample = 0;
 
-		if( dac[Square1] != NULL && config.apu_square1 )
+		if( dac[Square1] != NULL && dac[Square1]->ppu_cycles <= step_ppu_cycles && config.apu_square1 )
 			square1_sample = dac[Square1]->sample;
-		if( dac[Square2] != NULL && config.apu_square2 )
+		if( dac[Square2] != NULL && dac[Square2]->ppu_cycles <= step_ppu_cycles && config.apu_square2 )
 			square2_sample = dac[Square2]->sample;
-		if( dac[Triangle] != NULL && config.apu_triangle )
+		if( dac[Triangle] != NULL && dac[Triangle]->ppu_cycles <= step_ppu_cycles && config.apu_triangle )
 			triangle_sample = dac[Triangle]->sample;
+		if( dac[Noise] != NULL && dac[Noise]->ppu_cycles <= step_ppu_cycles && config.apu_noise )
+			noise_sample = dac[Noise]->sample;
+		if( dac[DMC] != NULL && dac[DMC]->ppu_cycles <= step_ppu_cycles && config.apu_dmc )
+			dmc_sample = dac[DMC]->sample;
 
-		sample += square_dac_outputs[square1_sample + square2_sample];
-		sample += tnd_dac_outputs[triangle_sample];
+		sample = 0;
 
+		index = square1_sample + square2_sample;
+		sample += square_dac_outputs[index];
+		index = 3*triangle_sample + 2*noise_sample + dmc_sample;
+		sample += tnd_dac_outputs[index];
+
+		/* If we got anything, then we should play silence (or not?) */
 		if( sample == 0 )
 			sample = audio_spec.silence;
 
