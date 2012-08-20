8ed3393e6df9a2c9b7e32dc423ced5df887ebc8b
Rodrigo Javier Tobar Carrizo <rtobar@csrg.inf.utfsm.cl>
2012-08-20 22:06:46 +0200

    Include libintl.h only when necessary

--- include/i18n.h
+++ include/i18n.h
@@ -1,17 +1,17 @@
 #ifndef i18n_h
 #define i18n_h
 
-#include <libintl.h>
 #if 0
  #define _(string)                         (string)
  #define N_(string)                        (string)
  #define textdomain(domain)
  #define bindtextdomain(package,directory)
 #else
+ #include <libintl.h>
  #define _(string)                         gettext(string)
  #define gettext_noop(string)              (string)
  #define N_(string)                        gettext_noop(string)
-#endif /* if 1 */
+#endif
 
 
 #endif /* i18n_h */
